923f087746a920fa85806374f4ec5bd7a553620e
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2024-07-05 19:28:32 -0400

    feat: se agrego un Referer para posibles futuros problemas

--- DetectorSebasCMS.py
+++ DetectorSebasCMS.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import re
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Referer': 'https://www.google.com'}
 url = input("Ingresa una URL: ")
 def quitarDirectorio(url):
     # Verificar si la URL contiene un directorio
