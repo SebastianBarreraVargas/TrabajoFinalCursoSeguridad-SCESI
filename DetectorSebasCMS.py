f6cca88fecec825d171b8ee03f47f492e9f401ab
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2024-07-08 23:56:46 -0400

    feat: Devuelve caracteristicas del header

--- DetectorSebasCMS.py
+++ DetectorSebasCMS.py
@@ -4,7 +4,10 @@ import random
 import re
 session = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 
-           'Referer': 'https://www.google.com'}
+           'Referer': 'https://www.google.com',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive'}
 url = input("Ingresa una URL: ")
 def quitarDirectorio(url):
     # Verificar si la URL contiene un directorio
